7e71f1c7161e392f5556e481c6e1f3b18b335655
chiman <rlaclghks123@naver.com>
2022-11-05 18:11:03 +0900

    Test: test calculator

---
+++ test/calculator.test.js
@@ -0,0 +1,54 @@
+const Calculator = require("../src/Calculator.js");
+
+describe("Calculator", () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+  test("add test", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  test("set Test", () => {
+    cal.set(50);
+    expect(cal.value).toBe(50);
+  });
+
+  test("clear Test", () => {
+    cal.set(50);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  test("add Test", () => {
+    cal.set(1);
+    cal.add(50);
+    expect(cal.value).toBe(51);
+  });
+
+  test("subtract Test", () => {
+    cal.set(51);
+    cal.subtract(50);
+    expect(cal.value).toBe(1);
+  });
+
+  test("subtract Test", () => {
+    cal.set(5);
+    cal.multiply(5);
+    expect(cal.value).toBe(25);
+  });
+
+  describe("divide", () => {
+    test("0/0=== NAN", () => {
+      cal.clear();
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    test("1/0=== Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+  });
+});
