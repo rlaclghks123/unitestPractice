ddcc26e03d4a189c53e9322f49d765920a179ee1
chiman <rlaclghks123@naver.com>
2022-11-06 13:13:56 +0900

    Test : mock function old version Test

---
+++ src/mock/mock.js
@@ -0,0 +1,6 @@
+function mock(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
+}
+module.exports = mock;
--- test/mock.test.js
+++ test/mock.test.js
@@ -1,1 +1,21 @@
+const mock = require("../src/mock/mock.js");
 
+describe("mockTest", () => {
+  test("Test 1 : old version - calls.length", () => {
+    const mockCallback = jest.fn((x) => 42 + x);
+    mock([0, 1], mockCallback);
+    expect(mockCallback.mock.calls.length).toBe(2);
+  });
+
+  test("Test 2 : old version - calls[0][0] ", () => {
+    const mockCallback = jest.fn((x) => 42 + x);
+    mock([0, 1], mockCallback);
+    expect(mockCallback.mock.calls[0][0]).toBe(0);
+  });
+
+  test("Test 2 : old version - calls[0][1] ", () => {
+    const mockCallback = jest.fn((x) => 42 + x);
+    mock([0, 1], mockCallback);
+    expect(mockCallback.mock.results[0].value).toBe(42);
+  });
+});
