d6f91a37a085af100ae844443f81fbd09c532e26
chiman <rlaclghks123@naver.com>
2022-11-08 11:39:48 +0900

    TDD 연습 - stack.pop() test

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -10,5 +10,12 @@ class Stack {
   push(item) {
     return this.array.push(item);
   }
+
+  pop() {
+    if (this.array.length === 0) {
+      throw new Error("when stack is Empty, can not use pop()");
+    }
+    return this.array.pop();
+  }
 }
 module.exports = Stack;
--- src/stack/test/stack.test.js
+++ src/stack/test/stack.test.js
@@ -15,4 +15,18 @@ describe("stack test", () => {
     stack.push(2);
     expect(stack.size()).toBe(2);
   });
+
+  describe("pop test", () => {
+    test("when array is empty, pop test", () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow("when stack is Empty, can not use pop()");
+    });
+
+    test("when stack is not empty, pop test", () => {
+      stack.push(1);
+      expect(stack.pop()).toBe(1);
+      expect(stack.size()).toBe(0);
+    });
+  });
 });
